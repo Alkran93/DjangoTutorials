8052698d73ee61b4fd9d0ecb147cc96699063c47
SofiaZapata <szapataz1@eafit.edu.co>
2025-08-14 22:22:48 -0500

    Tutorial de dependencias y sesiones

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
-from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView
+from .views import HomePageView, AboutPageView, ContactPageView, ProductIndexView, ProductShowView, ProductCreateView, CartView, CartRemoveAllView 
+from .views import ImageViewFactory
+from .utils import ImageLocalStorage
 
 urlpatterns= [
     path('', HomePageView.as_view(), name='home'),
@@ -8,4 +10,10 @@ urlpatterns= [
     path('contact/', ContactPageView.as_view(), name='contact'),
     path('products/', ProductIndexView.as_view(), name='index'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
-    path('product-create/', views.ProductCreateView.as_view(), name='product-create'),]
+    path('product-create/', views.ProductCreateView.as_view(), name='product-create'),
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'), 
+    path('image/', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_index'),
+    path('image/save', ImageViewFactory(ImageLocalStorage()).as_view(), name='image_save')
+    ]
